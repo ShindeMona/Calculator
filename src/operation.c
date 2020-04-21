83231ad1932a54388d7d60f8c4f3a8a7ccb168dd
Shinde Monali Ajinath <vp.monali@gmail.com>
2020-04-21 16:58:09 +0530

    Create operation.c

---
+++ src/operation.c
@@ -0,0 +1,20 @@
+int addi(int a,int b)
+{
+  return (a+b);
+}
+
+int sub(int a, int b)
+{
+
+return (a-b);
+}
+
+int multi(int a,int b)
+{
+return (a*b);
+}
+
+int divi(int a,int b)
+{
+return (a/b);
+}
